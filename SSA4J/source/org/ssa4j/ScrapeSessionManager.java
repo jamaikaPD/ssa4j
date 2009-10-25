e459a84c2a9409c65fef4239af7b79a047d94a53
raiglstorfer <raiglstorfer@7f6a27ac-bc1e-11de-a0e9-83d03c287164>
2009-10-25 09:02:42 +0000

    added fix

--- SSA4J/source/org/ssa4j/ScrapeSessionManager.java
+++ SSA4J/source/org/ssa4j/ScrapeSessionManager.java
@@ -6,8 +6,6 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.xml.ws.BindingType;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.ssa4j.ScrapeSessionVariable.BindType;
